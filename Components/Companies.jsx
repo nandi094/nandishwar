08ec8b50e55b77cec8abe7791024fd2c5c773e31
nandi094 <nandigowda094@gmail.com>
2024-07-25 09:18:13 +0530

    Adding all the JS files

---
+++ Components/Companies.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Companies(props) {
+		return(
+			<li>
+			<img src={props.image} alt={props.title} />
+			<h3>{props.title}</h3>
+			<p>{props.description}</p>
+			</li>
+		)
+}
---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import React from 'react';
+import { CORE_COMPANIES } from "../data.js";
+import Companies from "../Components/Companies";
+import TabButton from "../Components/TabBox.jsx";
+function App() {
+    const [selComp, setSelComp] = useState('click here');
+
+    function handleSelect(selectedComp) {
+        console.log(selectedComp);
+        setSelComp(selectedComp);
+    }
+    return (
+        <div>
+            <header>
+                <h1>Nandishwar C</h1>
+            </header>
+            <main>
+                <section id="core-concepts">
+                <h2>Companies Worked</h2>
+                    <ul>
+                        <Companies {...CORE_COMPANIES[0]} />
+                        <Companies {...CORE_COMPANIES[1]} />
+                        <Companies {...CORE_COMPANIES[2]} />
+                        <Companies {...CORE_COMPANIES[3]} />
+                    </ul>
+                    <section id="examples">
+                        <ul>
+                            <TabButton onSelect={() => handleSelect('LSEG')}>More Info</TabButton>
+                            <TabButton onSelect={() => handleSelect('KPMG')}>More Info</TabButton>
+                            <TabButton onSelect={() => handleSelect('JOYIT')}>More Info</TabButton>
+                            <TabButton onSelect={() => handleSelect('INFY')}>More Info</TabButton>
+                        </ul>
+                    </section>
+                </section>   
+               
+                <section id="tab-content">
+                    { selComp}
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default App;
